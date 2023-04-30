02d6a249b71f3703c88920e5d7b864daf8f0621c
Leo <leoperez.x@gmail.com>
2023-04-30 08:37:58 -0500

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+from nis import maps
+import streamlit as st
+
+APP_TITLE = "Reporte de accidentes vehículares de Palmira - 2020."
+APP_SUBTITLE = "Fuente: Datos abiertos, Alcaldía de Palmira."
+
+def main():
+    st.set_page_config(APP_TITLE)
+    st.title(APP_TITLE)
+    st.caption(APP_SUBTITLE)
+
+# LOAD DATA
+
+# DISPLAY FILTERS AND MAPS
+
+# DISPLAY METRICS
+
+if __name__=="__main__":
+    main()
