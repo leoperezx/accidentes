12faef9193a806b2b6ab8e8f2631587c036cf1e9
Leo <leoperez.x@gmail.com>
2023-05-05 22:24:17 -0500

    "Óptimizada, textos en markdown, no carga mapa"

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ APP_TITLE = "Reporte de accidentes vehículares de Palmira - 2020."
 APP_SUBTITLE = "Fuente: Datos abiertos, Alcaldía de Palmira."
 APP_SUBTITLE_MAP ="Mapa de todos los accidentes registrados de Palmira - 2020."
 APP_INTRO = "El actual reporte es realizado con la información suministrada por la secretaría de transito y transporte de la ciudad de Palmira, Valle, Colombia. Los datos sin procesar los puede encontrar [aquí](https://www.datos.gov.co/Transporte/Accidentes-de-transito-Palmira-2020/mg8y-amuh). Página oficial de 'Datos abiertos' del gobierno de Colombia. <br><br>El siguiente trabajo es un proyecto en proceso. Lo que quiere decir que es autónomo, no esta terminado, no es oficilal y es realizado a modo de práctica para el análisis de datos de forma autodidacta. Además, se estudia la programación de diferentes lenguajes, incluyendo varios framewokrs de forma autonoma utilizando solo el uso de Internet. <br><br>El primer paso fue tomar todo el dataset, leerlo y revisarlo. A continuación, se presenta el enlace que lleva a un archivo compartido de [Google Sheets](https://docs.google.com/spreadsheets/d/1Sjc3ELm89rNrAvtS5oZVNVF0ATXw8bG5O06IX_P2kGE/edit?usp=sharing) donde se realiza un resumen y algunas operaciones a los datos de la secretaría con algunas tablas dinámicas para hacer algúnos análisis."
-APP_FINAL = "Quedo atento a comentarios, preguntas, dudas, críticas constructivas, felicitaciones, ayuda y apoyo tanto en ideas para análisis como laboral<br><br>Todavía exisite mucha información sin analizar, muchas operaciones a realizar y funciones por aprender. Programo mientras aprendo. Veras muchos cambios. **Esto es de prueba y error**."
+APP_FINAL = "Quedo atento a comentarios, preguntas, dudas, críticas constructivas, felicitaciones, ayuda y apoyo tanto en ideas para análisis como laboral<br><br>Todavía exisite mucha información sin analizar, muchas operaciones a realizar y funciones por aprender. Programo mientras aprendo. Veras muchos cambios. **Esto es de prueba y error**.<br><br>&copy; | Contacto: [@leoperezx](https://twitter.com/leoperezx)" 
 
 df = pd.read_csv("add/dataset.csv")
 
@@ -29,7 +29,7 @@ def main():
     if st.button("Crear mapa"):
         fn.generarMapa(df)
     else: 
-        st.image(IMAGE, width=600, caption="Imagen provicional")
+        st.image(IMAGE, caption="Imagen provisional")
     st.markdown(APP_FINAL, True)
 
 
