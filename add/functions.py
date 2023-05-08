c5d00c5664ca1cf1f65a25306b7fd3ee6ebbafe6
Leo <leoperez.x@gmail.com>
2023-05-08 09:18:21 -0500

    "selección de veículos"

--- add/functions.py
+++ add/functions.py
@@ -68,7 +68,7 @@ def darFormatoGeo(df):
 
     return df
 
-def generarMapa(data):
+def generarMapa(data,options):
     '''
     Organiza la información de la base de datos para dibujar en un mapa 
     cada uno de los puntos georeferenciados para ubicar cada uno de los 
@@ -100,22 +100,23 @@ def generarMapa(data):
     TIPO_DE_SERVICIO,
     EMPRESA,,
 
-    
-
     '''
            
     from streamlit_folium import folium_static
     import folium
-    
-    # tiles: propiedad para cambiar el estilo del mapa.
+
+    data_filter = data.loc[data['CLASE_DE_VEHICULO'].isin(options)] 
+
     some_map = folium.Map(location=(3.535513,-76.297656),tiles="cartodbpositron", zoom_start=10)
 
     tool_tip="Click me!"    
 
-    for row in data.itertuples():
+
+    for row in data_filter.itertuples():
         pop_up=("<p>Choque de vehículo tipo: " + row.CLASE_DE_VEHICULO +"</p>" +
-                    "<p>Nivel de gravedad:" + row.GRAVEDAD + "</p>")
+                    "<p>Nivel de gravedad:" + row.GRAVEDAD + "</p>" + 
+                    "<p>Hipótesis del accidente:" + row.HIPOTESIS + "</p>")
         
-        folium.Marker([row.LAT,row.LONG], popup=pop_up, tooltip=tool_tip).add_to(some_map)
+        folium.Marker([row.LAT,row.LONG], popup=pop_up, tooltip=tool_tip, icon=folium.Icon(color='black',icon_color='#FFFF00')).add_to(some_map)
 
     return folium_static(some_map)
--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,9 @@ APP_TITLE = "Reporte de accidentes vehículares de Palmira - 2020."
 APP_SUBTITLE = "Fuente: Datos abiertos, Alcaldía de Palmira."
 APP_SUBTITLE_MAP ="Mapa de todos los accidentes registrados de Palmira - 2020."
 APP_INTRO = "El actual reporte es realizado con la información suministrada por la secretaría de transito y transporte de la ciudad de Palmira, Valle, Colombia. Los datos sin procesar los puede encontrar [aquí](https://www.datos.gov.co/Transporte/Accidentes-de-transito-Palmira-2020/mg8y-amuh). Página oficial de 'Datos abiertos' del gobierno de Colombia. <br><br>El siguiente trabajo es un proyecto en proceso. Lo que quiere decir que es autónomo, no esta terminado, no es oficilal y es realizado a modo de práctica para el análisis de datos de forma autodidacta. Además, se estudia la programación de diferentes lenguajes, incluyendo varios framewokrs de forma autonoma utilizando solo el uso de Internet. <br><br>El primer paso fue tomar todo el dataset, leerlo y revisarlo. A continuación, se presenta el enlace que lleva a un archivo compartido de [Google Sheets](https://docs.google.com/spreadsheets/d/1Sjc3ELm89rNrAvtS5oZVNVF0ATXw8bG5O06IX_P2kGE/edit?usp=sharing) donde se realiza un resumen y algunas operaciones a los datos de la secretaría con algunas tablas dinámicas para hacer algúnos análisis."
-APP_FINAL = "Quedo atento a comentarios, preguntas, dudas, críticas constructivas, felicitaciones, ayuda y apoyo tanto en ideas para análisis como laboral<br><br>Todavía exisite mucha información sin analizar, muchas operaciones a realizar y funciones por aprender. Programo mientras aprendo. Veras muchos cambios. **Esto es de prueba y error**.<br><br>&copy; | Contacto: [@leoperezx](https://twitter.com/leoperezx)" 
+APP_FINAL = "Quedo atento a comentarios, preguntas, dudas, críticas constructivas, felicitaciones, ayuda y apoyo tanto en ideas para análisis como laboral<br><br>Todavía exisite mucha información sin analizar, muchas operaciones a realizar y funciones por aprender. Programo mientras aprendo. Veras muchos cambios. **Esto es de prueba y error**."
 
 df = pd.read_csv("add/dataset.csv")
-
 
 
 def main():
@@ -26,10 +25,16 @@ def main():
     st.write("Dimenciones del dataset (filas, columnas): ",df.shape)
     st.write(df.head(10))
     st.subheader("Mapa interactivo de los accidentes - Palmira 2020")
+
+    options = st.multiselect('¿Qué automóviles desea consultar?',
+        ['AUTOMOVIL','BICICLETA','BUS','BUSETA','CAMION','CAMIONETA',
+        'CAMPERO','CICLISTA','MAQUINARIA','MICROBUS','MOTO','MOTOCARRO',
+        'NO APLICA','TRACTO CAMION','TRICICLO','VOLQUETA'],key="selection")
+    
     if st.button("Crear mapa"):
-        fn.generarMapa(df)
+        fn.generarMapa(df,options)
     else: 
-        st.image(IMAGE, caption="Imagen provisional")
+        st.image(IMAGE, width=600, caption="Imagen provicional")
     st.markdown(APP_FINAL, True)
 
 
